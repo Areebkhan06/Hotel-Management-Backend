01af8e1cfbc196452e1a4c53bdbdd7f3714b8855
Areeb khan <areebkhan4575@gmail.com>
2025-09-25 00:39:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin:"https://hotel-management-frontend-bi3byjci9-khans-projects-cfde2cb3.vercel.app"|| "http://localhost:5173", // fallback if not set
+    origin:"https://hotel-management-frontend-22b2.onrender.com"|| "http://localhost:5173", // fallback if not set
     credentials: true,
   })
 );
